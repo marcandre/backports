8b8dd029d3e868127cb7cf8452214bb938da469f
Marc-Andre Lafortune <github@marc-andre.ca>
2009-09-29 00:04:55 -0400

    Added Enumerable#chunk (1.9.2)

--- lib/backports/1.9/enumerable.rb
+++ lib/backports/1.9/enumerable.rb
@@ -5,4 +5,32 @@ module Enumerable
     each {|obj| block.call(obj, memo)}
     memo
   end unless method_defined? :each_with_object
+
+  def chunk(initial_state = nil, &original_block)
+    Enumerator.new do |yielder|
+      previous = Backports::Undefined
+      accumulate = []
+      block = initial_state.nil? ? original_block : Proc.new{|val| original_block.yield(val, initial_state.clone)}
+      each do |val|
+        case key = block.yield(val)
+        when nil, :_separator
+          next
+        when :_singleton
+          yielder.yield previous, accumulate unless accumulate.empty?
+          yielder.yield key, [val]
+          accumulate = []
+          previous = Backports::Undefined
+        when previous, Backports::Undefined
+          accumulate << val
+          previous = key
+        else
+          yielder.yield previous, accumulate unless accumulate.empty?
+          accumulate = [val]
+          previous = key
+        end
+      end
+      # what to do in case of a break?
+      yielder.yield previous, accumulate unless accumulate.empty?
+    end
+  end
 end
--- test/enumerable_test.rb
+++ test/enumerable_test.rb
@@ -7,7 +7,7 @@ class EnumerableTest < Test::Unit::TestCase
       should "conform to doc" do
         assert_equal 4, (1..4).count
         assert_equal 1, (1..4).count(3)
-        assert_equal 2, (1..4).count{|obj| obj > 2 } 
+        assert_equal 2, (1..4).count{|obj| obj > 2 }
       end
     end
 
@@ -16,21 +16,21 @@ class EnumerableTest < Test::Unit::TestCase
         assert_equal ["a", "b", "c", "a", "b", "c"], ('a'..'c').cycle(2).to_a
       end
     end
-    
+
     context "#drop" do
       should "conform to doc" do
         assert_equal [5, 8, 13], [ 1, 1, 2, 3, 5, 8, 13 ].drop(4)
         assert_equal [], [ 1, 1, 2, 3, 5, 8, 13 ].drop(99)
       end
-    
+
       should "work with enums" do
         assert_equal [14,15], (10...16).drop(4)
       end
     end
-  
+
     context "#drop_while" do
       should "conform to doc" do
-        assert_equal [8, 13], [ 1, 1, 2, 3, 5, 8, 13 ].drop_while {|item| item < 6 } 
+        assert_equal [8, 13], [ 1, 1, 2, 3, 5, 8, 13 ].drop_while {|item| item < 6 }
       end
 
       should "work with enums" do
@@ -42,13 +42,13 @@ class EnumerableTest < Test::Unit::TestCase
         assert_equal [], (10...16).drop_while {|item| true}
       end
     end
-    
+
     context "#each_cons" do
       should "conform to doc" do
         assert_equal [[1,2],[2,3],[3,4]], (1..4).each_cons(2).to_a
       end
     end
-    
+
     context "#each_slice" do
       should "conform to doc" do
         res = []
@@ -60,10 +60,10 @@ class EnumerableTest < Test::Unit::TestCase
 
     context "#each_with_index" do
       should "conform to doc" do
-        hash = Hash.new 
-        %w(cat dog wombat).each_with_index do |item, index| 
-          hash[item] = index 
-        end 
+        hash = Hash.new
+        %w(cat dog wombat).each_with_index do |item, index|
+          hash[item] = index
+        end
         assert_equal({"cat"=>0, "wombat"=>2, "dog"=>1}, hash)
       end
 
@@ -76,9 +76,9 @@ class EnumerableTest < Test::Unit::TestCase
 
     context "#each_with_object" do
       should "conform to doc" do
-        hash = %w(cat dog wombat).each_with_object({}) do |item, memo| 
-          memo[item] = item.upcase.reverse 
-        end 
+        hash = %w(cat dog wombat).each_with_object({}) do |item, memo|
+          memo[item] = item.upcase.reverse
+        end
         assert_equal({"cat"=>"TAC", "dog"=>"GOD", "wombat"=>"TABMOW"}, hash)
       end
     end
@@ -123,28 +123,28 @@ class EnumerableTest < Test::Unit::TestCase
 
     context "#max_by" do
       should "conform to doc" do
-        a = %w(albatross dog horse fox) 
+        a = %w(albatross dog horse fox)
         assert_equal "albatross" , a.max_by {|item| item.length }
         assert_equal "fox", a.max_by {|item| item.reverse }
       end
-    end  
+    end
 
     context "#min_by" do
       should "conform to doc" do
-        a = %w(albatross dog horse fox) 
+        a = %w(albatross dog horse fox)
         assert_equal "dog" , a.min_by {|item| item.length }
         assert_equal "horse", a.min_by {|item| item.reverse }
       end
-    end  
+    end
 
     context "#minmax" do
       should "conform to doc" do
-        a = %w(albatross dog horse) 
+        a = %w(albatross dog horse)
         assert_equal ["albatross", "horse"], a.minmax
         assert_equal ["dog", "albatross"], a.minmax {|a,b| a.length <=> b.length }
       end
     end
-    
+
     context "#one" do
       should "conform to doc" do
         assert_equal false, %w{ ant bear cat}.one? {|word| word.length >= 3}
@@ -152,25 +152,25 @@ class EnumerableTest < Test::Unit::TestCase
         assert_equal true,  [ nil, nil, 99 ].one?
       end
     end
-    
+
     context "#reverse_each" do
       should "work as expected" do
         assert_equal [4,3,2], (1..4).reverse_each.take(3)
       end
     end
-        
+
     context "#take" do
       should "conform to doc" do
         assert_equal [1, 2, 3], (1..7).take(3)
         assert_equal [["a", 1], ["b", 2]], { 'a'=>1, 'b'=>2, 'c'=>3 }.take(2)
       end
-    
+
       should "only consume what's needed" do
         assert_equal [], Enumerator.new(nil).take(0)
         assert_raises(NoMethodError) { Enumerator.new(nil).take(1) }
       end
     end
-  
+
     context "#take_while" do
       should "conform to doc" do
         assert_equal [1,2], (1..7).take_while {|item| item < 3 }
@@ -187,6 +187,34 @@ class EnumerableTest < Test::Unit::TestCase
         s = StringIO.new("Hello world!")
         assert_equal ["Hello", " wo", "rld!"], s.to_a("o")
       end
+    end
+
+    context "#chunk" do
+      context "given no argument" do
+        should "should chunk correctly even for non-consecutive but similar keys" do
+          e = (1..7).chunk{|i| (i/3) % 2}
+          assert_equal true, e.is_a?(Enumerator)
+          assert_equal [
+            [0, [1,2]],
+            [1, [3,4,5]],
+            [0, [6,7]]], e.to_a
+        end
+      end
+      context "given an initial_state argument" do
+        should "give a new copy" do
+          a = []
+          e = (1..3).chunk(a) do |i, x|
+            assert_equal true, x.empty?
+            x << :yo
+            :_singleton
+          end
+          assert_equal [
+            [:_singleton, [1]],
+            [:_singleton, [2]],
+            [:_singleton, [3]]], e.to_a
+        end
+      end
+
     end
 
   end
