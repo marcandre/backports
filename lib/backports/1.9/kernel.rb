e418a91bac844866d574325098da285a5a2173ee
Marc-Andre Lafortune <github@marc-andre.ca>
2009-11-03 18:07:24 -0500

    Object#public_send: Added new method (1.9)

--- lib/backports/1.9/kernel.rb
+++ lib/backports/1.9/kernel.rb
@@ -8,4 +8,13 @@ module Kernel
     end 
     require File.expand_path(relative_feature, File.dirname(file))
   end
+
+  def public_send(method, *args, &block)
+    if respond_to?(method) && !protected_methods.include?(method.to_s)
+      send(method, *args, &block)
+    else
+      :foo.generate_a_no_method_error_in_preparation_for_method_missing  # otherwise a NameError might be returned
+      method_missing(method.to_sym, *args, &block)
+    end
+  end unless method_defined? :public_send
 end
