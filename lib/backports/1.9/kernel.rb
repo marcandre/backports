ad511b6584ffbb96fdaa4ef6f83e12d213b2d9b9
Marc-Andre Lafortune <github@marc-andre.ca>
2009-11-04 00:57:03 -0500

    Passing rubyspecs doesn't mean there aren't any stupid bugs

--- lib/backports/1.9/kernel.rb
+++ lib/backports/1.9/kernel.rb
@@ -21,7 +21,8 @@ module Kernel
     if respond_to?(method) && !protected_methods.include?(method.to_s)
       send(method, *args, &block)
     else
-      :foo.generate_a_no_method_error_in_preparation_for_method_missing  # otherwise a NameError might be returned
+      :foo.generate_a_no_method_error_in_preparation_for_method_missing rescue nil
+      # otherwise a NameError might be raised when we call method_missing ourselves
       method_missing(method.to_sym, *args, &block)
     end
   end unless method_defined? :public_send
