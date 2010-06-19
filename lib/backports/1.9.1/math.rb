6cbfd6dcea56148a4ce87cbccb4221b9bd3eae99
Arto Bendiken <arto@bendiken.net>
2010-06-19 12:41:41 +0800

    1.9.1: Implemented the extended Math.log(n, b) method and the missing Math.log2 method.

---
+++ lib/backports/1.9.1/math.rb
@@ -0,0 +1,27 @@
+module Math
+  class << self
+    # Standard in Ruby 1.9. See official documentation[http://ruby-doc.org/core-1.9/classes/Math.html]
+    unless (log(2, 2) rescue false)
+      def log_with_optional_base(numeric, base = Backports::Undefined)
+        if base.equal?(Backports::Undefined)
+          # Math.log(n) in 1.9.1 no longer accepts string arguments as it
+          # did on 1.8.x, but we won't risk redefining existing behavior
+          # when called with just one argument.
+          log_without_optional_base(numeric)
+        else
+          # Math.log(n, b) in 1.9.1 does not accept string arguments:
+          raise TypeError, "TypeError: can't convert String into Float" if numeric.is_a?(String) || base.is_a?(String)
+          log_without_optional_base(numeric) / log_without_optional_base(base)
+        end
+      end
+      Backports.alias_method_chain self, :log, :optional_base
+    end
+
+    # Standard in Ruby 1.9. See official documentation[http://ruby-doc.org/core-1.9/classes/Math.html]
+    def log2(numeric)
+      # Math.log2(n) in 1.9.1 does not accept string arguments:
+      raise TypeError, "TypeError: can't convert String into Float" if numeric.is_a?(String)
+      log(numeric) / log(2)
+    end unless method_defined? :log2
+  end
+end
