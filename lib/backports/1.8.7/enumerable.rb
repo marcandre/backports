421cb101f4fc6bc7a20972fb7d3b8f0bbd403c88
Marc-Andre Lafortune <github@marc-andre.ca>
2010-11-26 13:23:47 -0500

    1.8.7: Optimize Enumerable#{min|max}_by

--- lib/backports/1.8.7/enumerable.rb
+++ lib/backports/1.8.7/enumerable.rb
@@ -125,15 +125,25 @@ module Enumerable
   end
 
   # Standard in Ruby 1.8.7+. See official documentation[http://ruby-doc.org/core-1.9/classes/Enumerable.html]
-  def max_by(&block)
+  def max_by
     return to_enum(:max_by) unless block_given?
-    minmax_by(&block)[1]
+    max_object, max_result = nil, MOST_EXTREME_OBJECT_EVER
+    each do |object|
+      result = yield object
+      max_object, max_result = object, result if max_result < result
+    end
+    max_object
   end unless method_defined? :max_by
 
   # Standard in Ruby 1.8.7+. See official documentation[http://ruby-doc.org/core-1.9/classes/Enumerable.html]
-  def min_by(&block)
+  def min_by
     return to_enum(:min_by) unless block_given?
-    minmax_by(&block).first
+    min_object, min_result = nil, MOST_EXTREME_OBJECT_EVER
+    each do |object|
+      result = yield object
+      min_object, min_result = object, result if min_result > result
+    end
+    min_object
   end unless method_defined? :min_by
 
   # Standard in Ruby 1.8.7+. See official documentation[http://ruby-doc.org/core-1.9/classes/Enumerable.html]
