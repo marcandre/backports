f52abc9fea1e3c4081f9e0a096001fdcc2111ac1
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-12-01 17:11:20 +0100

    add 1.9.3 support

--- lib/backports/1.9.rb
+++ lib/backports/1.9.rb
@@ -1,4 +1,4 @@
 # require this file to load all the backports of Ruby
 
 require File.expand_path(File.dirname(__FILE__) + "/tools")
-Backports.require_relative "1.9.2"
+Backports.require_relative "1.9.3"
