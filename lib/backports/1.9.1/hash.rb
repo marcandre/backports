c6aed5854ed9778a9b639dc801cadebda69f0d05
Marc-Andre Lafortune <github@marc-andre.ca>
2011-12-02 01:11:24 -0500

    1.9.1: Backport Hash#assoc & Hash#rassoc, inspired by a patch from Konstantin Haase

--- lib/backports/1.9.1/hash.rb
+++ lib/backports/1.9.1/hash.rb
@@ -11,4 +11,22 @@ class Hash
   def default_proc=(proc)
     replace(Hash.new(&Backports.coerce_to(proc, Proc, :to_proc)).merge!(self))
   end unless method_defined? :default_proc=
+
+  # Standard in Ruby 1.9. See official documentation[http://ruby-doc.org/core-1.9/classes/Hash.html]
+  def assoc(key)
+    val = fetch(key) do
+      return find do |k, v|
+        [k, v] if k == key
+      end
+    end
+    [key, val]
+  end unless method_defined? :assoc
+
+
+  # Standard in Ruby 1.9. See official documentation[http://ruby-doc.org/core-1.9/classes/Hash.html]
+  def rassoc(value)
+    k = key(value)
+    v = fetch(k){return nil}
+    [k, fetch(k)] if k || v == value
+  end unless method_defined? :rassoc
 end
