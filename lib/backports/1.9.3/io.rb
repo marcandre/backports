4013a5f29e1722e4f8e23c573fa97dbce2761d01
Marc-Andre Lafortune <github@marc-andre.ca>
2012-03-25 02:32:34 -0400

    Add IO#advise (1.9.3)

--- lib/backports/1.9.3/io.rb
+++ lib/backports/1.9.3/io.rb
@@ -1,3 +1,23 @@
+class IO
+  # Standard in Ruby 1.9.3 See official documentation[http://ruby-doc.org/core-1.9.3/IO.html#method-i-advise]
+  # We're only for a platform not implementing advise, so we return nil.
+  def advise(advice, offset=0, len=0)
+    case advice
+    when  :normal,
+          :sequential,
+          :random,
+          :willneed,
+          :dontneed,
+          :noreuse
+      return nil
+    when Symbol
+      raise NotImplementedError, "Unsupported advice #{advice}"
+    else
+      raise TypeError, "advice must be a Symbol"
+    end
+  end unless method_defined? :advise
+end
+  
 class << IO
   # Standard in Ruby 1.9.3 See official documentation[http://ruby-doc.org/core-1.9.3/IO.html#method-c-write]
   def write(name, string, offset = nil, options = Backports::Undefined)
