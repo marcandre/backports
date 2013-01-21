617a09bd92e4c4975319e696b940924ff2a63e6d
Marc-Andre Lafortune <github@marc-andre.ca>
2013-01-21 15:52:38 -0500

    Fix header so require 'backports/2.0.0' work

--- lib/backports/2.0.0.rb
+++ lib/backports/2.0.0.rb
@@ -1,4 +1,5 @@
 # require this file to load all the backports up to Ruby 2.0.0
 
+require File.expand_path(File.dirname(__FILE__) + "/tools")
 Backports.require_relative "1.9"
 Backports.require_relative_dir "2.0.0"
