710b70e54fd8f166cf120161d3c919fb42b9d6d8
Marc-Andre Lafortune <github@marc-andre.ca>
2013-02-24 20:42:33 -0500

    v3.0.1: Fix embarassing mistakes. Thanks to viking. [#69]

--- lib/backports/2.0.0/hash.rb
+++ lib/backports/2.0.0/hash.rb
@@ -1,16 +1,20 @@
 class Hash
   def to_h
     self
-  end unless method_defined :to_h
+  end unless method_defined? :to_h
 
-  def default_proc_with_nil=(proc)
-    if proc == nil
-      self.default = nil
-      self
-    else
-      self.default_proc_without_nil=(proc)
+  begin
+    {}.default_proc = nil
+  rescue
+    def default_proc_with_nil=(proc)
+      if proc == nil
+        self.default = nil
+        self
+      else
+        self.default_proc_without_nil=(proc)
+      end
     end
-  end if ({}.default_proc = nil rescue true)
+  end
 end
 
-Backports.alias_method(ENV, :to_h, :to_hash)
+Backports.alias_method(ENV.singleton_class, :to_h, :to_hash)
