77b658c51be80535120cb44fc628fa2b9d2f19a5
Marc-Andre Lafortune <github@marc-andre.ca>
2013-02-24 22:54:52 -0500

    Enumerable#each_with_index(*) is not a feature of 1.8.7! Move to 1.9.1

--- lib/backports/1.8.7/enumerable.rb
+++ lib/backports/1.8.7/enumerable.rb
@@ -58,17 +58,6 @@ module Enumerable
   end unless method_defined? :drop_while
 
   # Standard in Ruby 1.8.7+. See official documentation[http://ruby-doc.org/core-1.9/classes/Enumerable.html]
-  if instance_method(:each_with_index).arity.zero?
-    def each_with_index_with_optional_args_and_block(*args)
-      return to_enum(:each_with_index, *args) unless block_given?
-      idx = 0
-      each(*args) { |o| yield(o, idx); idx += 1 }
-      self
-    end
-    Backports.alias_method_chain self, :each_with_index, :optional_args_and_block
-  end
-
-  # Standard in Ruby 1.8.7+. See official documentation[http://ruby-doc.org/core-1.9/classes/Enumerable.html]
   def find_index(obj = Backports::Undefined)
     if obj != Backports::Undefined
       each_with_index do |element, i|
