7a5d59c5d527d602e628b410ef0b6cab4ade7a6f
Marc-Andre Lafortune <github@marc-andre.ca>
2013-03-03 23:52:10 -0500

    Tweak Complex#to_r

--- lib/backports/1.9.2/complex/to_r.rb
+++ lib/backports/1.9.2/complex/to_r.rb
@@ -2,7 +2,7 @@ if Object.const_defined? :Complex
   unless Complex.method_defined? :to_r
     class Complex
       def to_r
-        raise "Can't convert #{self} into Rational" unless imag.eql?(0)
+        raise RangeError, "Can't convert #{self} into Rational" unless imag.eql?(0)
         real.to_r
       end
     end
