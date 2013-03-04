2bd93a9df464f5674ca53813e0b0b54e4a5a263b
Marc-Andre Lafortune <github@marc-andre.ca>
2013-03-03 21:53:43 -0500

    Fix File.expand_path

--- lib/backports/1.9.1/file/expand_path.rb
+++ lib/backports/1.9.1/file/expand_path.rb
@@ -1,5 +1,5 @@
 if RUBY_VERSION < '1.9'
   require 'backports/tools'
 
-  Backports.convert_first_argument_to_path File, :expand_path
+  Backports.convert_all_arguments_to_path File, :expand_path, 0
 end
