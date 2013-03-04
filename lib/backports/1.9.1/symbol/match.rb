e640095ca741f0f31dc8ce14b96006e967b40417
Marc-Andre Lafortune <github@marc-andre.ca>
2013-03-03 23:42:44 -0500

    Fix Symbol#=~

--- lib/backports/1.9.1/symbol/match.rb
+++ lib/backports/1.9.1/symbol/match.rb
@@ -1,11 +1,9 @@
 unless Symbol.method_defined? :match
   class Symbol
     def match(with)
-      to_s.match(with)
+      to_s =~ with
     end
+
+    alias_method :=~, :match
   end
 end
-
-require 'backports/tools'
-
-Backports.alias_method Symbol, :=~, :match
