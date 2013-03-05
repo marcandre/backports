7c17d2225ce31f7ee7f82f95d74faeecd92febd5
Marc-Andre Lafortune <github@marc-andre.ca>
2013-03-05 00:44:16 -0500

    Fix Hash[]

--- lib/backports/1.8.7/hash/constructor.rb
+++ lib/backports/1.8.7/hash/constructor.rb
@@ -1,15 +1,25 @@
 unless (Hash[[[:test, :test]]] rescue false)
+  require 'backports/tools'
+
   class << Hash
     alias_method :constructor_without_key_value_pair_form, :[]
     def [](*args)
-      return constructor_without_key_value_pair_form(*args) unless args.length == 1 && args.first.is_a?(Array)
-      h = {}
-      args.first.each do |arr|
-        next unless arr == Backports.is_array?(arr)
-        next unless (1..2).include? arr.size
-        h[arr.at(0)] = arr.at(1)
+      if args.length == 1
+        arg = args.first
+        if (h = Backports.try_convert(arg, Hash, :to_hash))
+          return allocate.replace(h)
+        end
+        if (kvps = Backports.is_array?(arg))
+          h = {}
+          kvps.each do |elem|
+            next unless arr = Backports.is_array?(elem)
+            next unless (1..2).include? arr.size
+            h[arr.at(0)] = arr.at(1)
+          end
+          return h
+        end
       end
-      h
+      constructor_without_key_value_pair_form(*args)
     end
   end
 end
