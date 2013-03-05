e2fc24f13c7130dab80f696dfe5664055846ee12
Marc-Andre Lafortune <github@marc-andre.ca>
2013-03-04 22:52:30 -0500

    Tweak File.expand_path

--- lib/backports/1.9.1/file/expand_path.rb
+++ lib/backports/1.9.1/file/expand_path.rb
@@ -1,5 +1,14 @@
 if RUBY_VERSION < '1.9'
   require 'backports/tools'
 
-  Backports.convert_all_arguments_to_path File, :expand_path, 0
+  class << File
+    def expand_path_with_potential_to_path(file, dir = nil)
+      raise ArgumentError, 'home not set' if file == '~' && ENV["HOME"] == ''
+      expand_path_without_potential_to_path(
+        Backports.convert_path(file),
+        dir == nil ? dir : Backports.convert_path(dir)
+      )
+    end
+    Backports.alias_method_chain self, :expand_path, :potential_to_path
+  end
 end
