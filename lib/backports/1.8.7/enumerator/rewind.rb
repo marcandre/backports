ba52f83ce6978ae9e10276f59b83de1f47525259
Marc-Andre Lafortune <github@marc-andre.ca>
2013-03-04 19:08:17 -0500

    Tweak Enumerator#rewind

--- lib/backports/1.8.7/enumerator/rewind.rb
+++ lib/backports/1.8.7/enumerator/rewind.rb
@@ -3,7 +3,6 @@ unless Object.const_defined? :Enumerator
   unless Enumerable::Enumerator.method_defined? :rewind
     class Enumerable::Enumerator
       def rewind
-        @object.rewind if @object.respond_to? :rewind
         require 'generator'
         @generator ||= ::Generator.new(self)
         @generator.rewind
