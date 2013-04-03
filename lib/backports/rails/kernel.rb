06fd882fc793789cadf57ab88d973efafafe201c
Marc-Andre Lafortune <github@marc-andre.ca>
2013-04-02 21:52:45 -0400

    Rails define `try` on Object

--- lib/backports/rails/kernel.rb
+++ lib/backports/rails/kernel.rb
@@ -1,4 +1,4 @@
-module Kernel
+class Object
   # Standard in rails. See official documentation[http://api.rubyonrails.org/classes/Object.html]
   def try(method_id, *args, &block)
     send(method_id, *args, &block) unless self.nil?
