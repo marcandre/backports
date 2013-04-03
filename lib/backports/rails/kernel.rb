5a09e6d833e5e69d5804e886c5307f39767c904f
Marc-Andre Lafortune <github@marc-andre.ca>
2013-04-02 21:52:30 -0400

    Remove obsolete returning

--- lib/backports/rails/kernel.rb
+++ lib/backports/rails/kernel.rb
@@ -3,10 +3,4 @@ module Kernel
   def try(method_id, *args, &block)
     send(method_id, *args, &block) unless self.nil?
   end unless method_defined? :try
-
-  # Standard in rails. See official documentation[http://api.rubyonrails.org/classes/Object.html]
-  def returning(obj)
-    yield obj
-    obj
-  end unless method_defined? :returning
 end
