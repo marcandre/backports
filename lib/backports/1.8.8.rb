928ee7e7b6f68b12b7b0df3f9b5e3eead46e8022
Marc-Andre Lafortune <github@marc-andre.ca>
2014-01-30 11:12:35 -0500

    Reorganize README

--- lib/backports/1.8.8.rb
+++ lib/backports/1.8.8.rb
@@ -1,2 +1,3 @@
-# require this file to load all the backports up to Ruby 1.8.8
+# Ruby 1.8.8 has been officially cancelled. As of version 2.2 of backports, <tt>require "backports/1.8.8"</tt>
+# does nothing more than require 1.8.7 backports
 require 'backports/1.8.7'
