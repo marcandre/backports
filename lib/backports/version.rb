54941c9203a6694f4c21141572e1d1c8f8dc38ac
Marc-Andre Lafortune <github@marc-andre.ca>
2015-08-01 22:59:54 -0400

    Release 3.6.6

--- lib/backports/version.rb
+++ lib/backports/version.rb
@@ -1,3 +1,3 @@
 module Backports
-  VERSION = "3.6.5" unless const_defined? :VERSION # the guard is against a redefinition warning that happens on Travis
+  VERSION = "3.6.6" unless const_defined? :VERSION # the guard is against a redefinition warning that happens on Travis
 end
