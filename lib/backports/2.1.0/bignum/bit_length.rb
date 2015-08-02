cabc87f002f507078649f70d88d8798bff926d86
Marc-Andre Lafortune <github@marc-andre.ca>
2015-08-01 22:40:55 -0400

    Fix Bignum#bit_length correctly [#95]

--- lib/backports/2.1.0/bignum/bit_length.rb
+++ lib/backports/2.1.0/bignum/bit_length.rb
@@ -2,16 +2,17 @@ unless Bignum.method_defined? :bit_length
   require 'backports/2.0.0/range/bsearch'
   class Bignum
     def bit_length
-      # We use the fact that bignums use the minimum number of bytes necessary
-      # So we have (size - 1) * 8 < bit_length <= size * 8
-      n = 8 * (size - 1)
+      # We use the fact that bignums use the minimum number of "words" necessary
+      # where "words" is some number of bytes <= to the size of a fixnum
+      # So we have (size - word_size) * 8 < bit_length <= size * 8
+      n = 8 * (size - 42.size)
       smaller = self >> n
       if smaller >= 0
         smaller += 1
       else
         smaller = -smaller
       end
-      n + (1..8).bsearch{|i| smaller <= (1 << i) }
+      n + (1..8 * 42.size).bsearch{|i| smaller <= (1 << i) }
     end
   end
 end
