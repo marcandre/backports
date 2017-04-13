5e6bcd0bdc06ed3f49cfb569a99f58d35cfecc99
Yousuf Jukaku <yjukaku@levelsetsolutions.com>
2017-04-13 13:51:16 -0400

    Fix String/Regexp #match? use fiber to avoid setting $~

--- lib/backports/2.4.0/regexp/match_bool.rb
+++ lib/backports/2.4.0/regexp/match_bool.rb
@@ -1,7 +1,11 @@
 unless Regexp.method_defined? :match?
   class Regexp
     def match?(str, pos = nil)
-      !match(arg, pos).nil?
+      # Fiber to avoid setting $~
+      f = Fiber.new do
+        !match(arg, pos).nil?
+      end
+      f.resume
     end
   end
 end
--- lib/backports/2.4.0/string/match_bool.rb
+++ lib/backports/2.4.0/string/match_bool.rb
@@ -1,7 +1,11 @@
 unless String.method_defined? :match?
   class String
     def match?(str, pos = nil)
-      !match(str, pos).nil?
+      # Fiber to avoid setting $~
+      f = Fiber.new do
+        !match(arg, pos).nil?
+      end
+      f.resume
     end
   end
 end
