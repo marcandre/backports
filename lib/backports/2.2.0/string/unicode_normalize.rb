c834fd4715b3ccbedb990bc618ae772b31880421
Marc-Andre Lafortune <github@marc-andre.ca>
2017-04-26 12:53:28 -0400

    Guard unicode_normalize against encoding unaware regexp engines like 1.8.7

--- lib/backports/2.2.0/string/unicode_normalize.rb
+++ lib/backports/2.2.0/string/unicode_normalize.rb
@@ -1,28 +1,29 @@
 # Adapted from CRuby
-# Copyright Ayumu Nojima (野島 歩) and Martin J. Dürst (duerst@it.aoyama.ac.jp)
 
 unless String.method_defined? :unicode_normalize
-  class String
-    def unicode_normalize(form = :nfc)
-      require 'backports/tools/normalize.rb' unless defined? UnicodeNormalize
-      ## The following line can be uncommented to avoid repeated checking for
-      ## UnicodeNormalize. However, tests didn't show any noticeable speedup
-      ## when doing this. This comment also applies to the commented out lines
-      ## in String#unicode_normalize! and String#unicode_normalized?.
-      # String.send(:define_method, :unicode_normalize, ->(form = :nfc) { UnicodeNormalize.normalize(self, form) } )
-      UnicodeNormalize.normalize(self, form)
-    end
+  if (Regexp.compile("[\u{11100}-\u{11102}]") rescue false)
+    class String
+      def unicode_normalize(form = :nfc)
+        require 'backports/tools/normalize.rb' unless defined? UnicodeNormalize
+        ## The following line can be uncommented to avoid repeated checking for
+        ## UnicodeNormalize. However, tests didn't show any noticeable speedup
+        ## when doing this. This comment also applies to the commented out lines
+        ## in String#unicode_normalize! and String#unicode_normalized?.
+        # String.send(:define_method, :unicode_normalize, ->(form = :nfc) { UnicodeNormalize.normalize(self, form) } )
+        UnicodeNormalize.normalize(self, form)
+      end
 
-    def unicode_normalize!(form = :nfc)
-      require 'backports/tools/normalize.rb' unless defined? UnicodeNormalize
-      # String.send(:define_method, :unicode_normalize!, ->(form = :nfc) { replace(unicode_normalize(form)) } )
-      replace(unicode_normalize(form))
-    end
+      def unicode_normalize!(form = :nfc)
+        require 'backports/tools/normalize.rb' unless defined? UnicodeNormalize
+        # String.send(:define_method, :unicode_normalize!, ->(form = :nfc) { replace(unicode_normalize(form)) } )
+        replace(unicode_normalize(form))
+      end
 
-    def unicode_normalized?(form = :nfc)
-      require 'backports/tools/normalize.rb' unless defined? UnicodeNormalize
-      # String.send(:define_method, :unicode_normalized?, ->(form = :nfc) { UnicodeNormalize.normalized?(self, form) } )
-      UnicodeNormalize.normalized?(self, form)
+      def unicode_normalized?(form = :nfc)
+        require 'backports/tools/normalize.rb' unless defined? UnicodeNormalize
+        # String.send(:define_method, :unicode_normalized?, ->(form = :nfc) { UnicodeNormalize.normalized?(self, form) } )
+        UnicodeNormalize.normalized?(self, form)
+      end
     end
   end
 end
