d0a72a99820d84a90b95fc65f3dd3138588c4a4d
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-06 23:04:07 -0400

    v3.9.1

--- lib/backports/version.rb
+++ lib/backports/version.rb
@@ -1,3 +1,3 @@
 module Backports
-  VERSION = "3.9.0" unless const_defined? :VERSION # the guard is against a redefinition warning that happens on Travis
+  VERSION = "3.9.1" unless const_defined? :VERSION # the guard is against a redefinition warning that happens on Travis
 end
