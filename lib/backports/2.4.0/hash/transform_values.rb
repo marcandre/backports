b39355d03cb81b2c0af7ce8809e1dd8730da947f
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-17 02:14:07 -0400

    Tweak transform_values! for improved specs

--- lib/backports/2.4.0/hash/transform_values.rb
+++ lib/backports/2.4.0/hash/transform_values.rb
@@ -10,6 +10,9 @@ class Hash
 
   def transform_values!
     return to_enum(:transform_values!){ size } unless block_given?
-    replace(transform_values{|i| yield i})
+    reject!{} if frozen? # Force error triggerring if frozen, in case of empty array
+    each do |key, value|
+      self[key] = yield value
+    end
   end unless method_defined? :transform_values!
 end
