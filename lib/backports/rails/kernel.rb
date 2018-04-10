6da7d4ca53015aa6c4e2c6f974e7d8f22b9c1307
Marc-Andre Lafortune <github@marc-andre.ca>
2018-04-10 16:33:27 -0400

    Update Rails' try [#115]

--- lib/backports/rails/kernel.rb
+++ lib/backports/rails/kernel.rb
@@ -1,6 +1,16 @@
-class Object
-  # Standard in rails. See official documentation[http://api.rubyonrails.org/classes/Object.html]
-  def try(method_id, *args, &block)
-    send(method_id, *args, &block) unless self.nil?
-  end unless method_defined? :try
+# frozen_string_literal: true
+
+# From ActiveSupport
+unless Object.method_defined? :try
+  class Object
+    def try(*a, &b)
+      try!(*a, &b) if a.empty? || respond_to?(a.first)
+    end
+  end
+
+  class NilClass
+    def try(*args)
+      nil
+    end
+  end
 end
