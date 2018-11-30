229b4fe0f9f30c2fcad86b8534514553edee98bb
Onni Hakala <onni@keksi.io>
2018-11-30 08:30:02 +0200

    Kernel#then: Don't add then if it's already defined

--- lib/backports/2.6.0/kernel/then.rb
+++ lib/backports/2.6.0/kernel/then.rb
@@ -1,11 +1,13 @@
 module Kernel
-  if method_defined? :yield_self
-    alias_method :then, :yield_self
-  else
-    # Run the same code as in yield_self if user only added then
-    def then
-      return to_enum(__method__) { 1 } unless block_given?
-      yield self
+  unless method_defined? :then
+    if method_defined? :yield_self
+      alias_method :then, :yield_self
+    else
+      # Run the same code as in yield_self if user only added then
+      def then
+        return to_enum(__method__) { 1 } unless block_given?
+        yield self
+      end
     end
   end
 end
