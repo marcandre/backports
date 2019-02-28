2747e1a866d159404321b0c57e03313f8068ea3f
Marc-Andre Lafortune <github@marc-andre.ca>
2019-02-28 11:46:20 -0500

    latest is 2.6 [#131]

--- lib/backports/latest.rb
+++ lib/backports/latest.rb
@@ -1,2 +1,4 @@
 # require this file to load all the backports
-require 'backports/2.5.0'
+# NOTE: This is NOT recommended.
+# Best to require the specific backports you need
+require 'backports/2.6.0'
