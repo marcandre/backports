edd1109ae8865d58daf34d256bcf075520f5ffdf
Thomas Roehr <thomas.roehr@dfki.de>
2019-06-07 10:47:11 +0200

    Fix version guard
    Currently, the use of Mixins might trigger wrong setting of version information.
    See examples below.
    VERSION should be set if not already defined on Backports.
    
    require 'fileutils'
    include Fileutils
    require 'backports/version'
    
    which boils down to a sample test case:
    
    module Test
        VERSION = "1.0.0"
    end
    include Test
    require 'backports'
    Backports::VERSION
    
    NameError (uninitialized constant Backports::VERSION)
    Did you mean?  VERSION

--- lib/backports/version.rb
+++ lib/backports/version.rb
@@ -1,3 +1,3 @@
 module Backports
-  VERSION = "3.15.0" unless const_defined? :VERSION # the guard is against a redefinition warning that happens on Travis
+  VERSION = "3.15.0" unless Backports.constants.include? :VERSION # the guard is against a redefinition warning that happens on Travis
 end
