4fd91fd41d724b3d3b818b63bbea0e09701f39d3
zverok <zverok.offline@gmail.com>
2020-01-30 13:07:26 +0200

    Fix Time for Ruby < 2.4

--- lib/backports/2.7.0/time/ceil.rb
+++ lib/backports/2.7.0/time/ceil.rb
@@ -1,8 +1,9 @@
 unless Time.method_defined?(:ceil)
   class Time
     def ceil(ndigits = 0)
-      s = (sec + subsec)
-      change = s.ceil(ndigits) - s
+      # Imitate value.ceil(ndigits) which does not exist in Ruby < 2.4
+      sceil = (subsec * 10**ndigits).ceil.to_r / 10**ndigits
+      change = sceil - subsec
       self + change
     end
   end
--- lib/backports/2.7.0/time/floor.rb
+++ lib/backports/2.7.0/time/floor.rb
@@ -1,9 +1,7 @@
 unless Time.method_defined?(:floor)
   class Time
     def floor(ndigits = 0)
-      s = (sec + subsec)
-      change = s - s.floor(ndigits)
-      self - change
+      self - subsec.modulo(10**-ndigits)
     end
   end
 end
