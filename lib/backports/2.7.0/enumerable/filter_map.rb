45fc91ac5cd19dc73f4815b88c4ec38ddeeccda3
zverok <zverok.offline@gmail.com>
2020-02-03 19:29:55 +0200

    Review comment fixes

--- lib/backports/2.7.0/enumerable/filter_map.rb
+++ lib/backports/2.7.0/enumerable/filter_map.rb
@@ -1,7 +1,9 @@
+require 'backports/1.9.1/enumerable/each_with_object' unless Enumerable.method_defined? :each_with_object
+
 unless Enumerable.method_defined? :filter_map
   module Enumerable
     def filter_map
-      return to_enum(__method__) unless block_given?
+      return to_enum(:filter_map) unless block_given?
 
       each_with_object([]) { |item, res|
         processed = yield(item)
