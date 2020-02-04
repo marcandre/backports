fb325b1f6fb90c849aedc5bf0aa9781ff902d486
Marc-Andre Lafortune <github@marc-andre.ca>
2020-02-03 22:03:29 -0600

    Fix backports for 1.8.6

--- lib/backports/2.4.0/enumerable/sum.rb
+++ lib/backports/2.4.0/enumerable/sum.rb
@@ -1,6 +1,10 @@
 module Enumerable
-  def sum(accumulator = 0, &block)
-    values = block_given? ? map(&block) : self
-    values.inject(accumulator, :+)
-  end unless method_defined? :sum
+  unless method_defined? :sum
+    require 'backports/1.8.7/enumerable/inject'
+
+    def sum(accumulator = 0, &block)
+      values = block_given? ? map(&block) : self
+      values.inject(accumulator, :+)
+    end
+  end
 end
