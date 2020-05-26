b722340bf1c9fc61a0d0884ff891b96203e6c900
zverok <zverok.offline@gmail.com>
2020-05-26 21:00:49 +0300

    How about now?

--- lib/backports/2.0.0.rb
+++ lib/backports/2.0.0.rb
@@ -1,3 +1,8 @@
 # require this file to load all the backports up to Ruby 2.0.0
 require 'backports/1.9'
-Backports.require_relative_dir if RUBY_VERSION < '2.0'
+
+if RUBY_VERSION < '2.0'
+  Backports.warned[:require_std_lib] = true
+  require "backports/std_lib"
+  Backports.require_relative_dir
+end
