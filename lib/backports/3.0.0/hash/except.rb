8108a29827c379524848c166368e020c6994cf94
Marc-Andre Lafortune <github@marc-andre.ca>
2020-12-15 16:24:57 -0500

    Add {Hash|ENV}#except (3.0)

---
+++ lib/backports/3.0.0/hash/except.rb
@@ -0,0 +1,10 @@
+class Hash
+  def except(*keys)
+    if keys.size > 4 && size > 4 # index if O(m*n) is big
+      h = {}
+      keys.each { |key| h[key] = true }
+      keys = h
+    end
+    reject { |key, _value| keys.include? key}
+  end
+end unless Hash.method_defined? :except
