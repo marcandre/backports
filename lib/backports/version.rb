a3983da1236af1af7836cc8a5b101fd978a9e773
Marc-André Lafortune <github@marc-andre.ca>
2021-12-27 16:35:01 -0500

    v3.22.0

--- lib/backports/version.rb
+++ lib/backports/version.rb
@@ -1,3 +1,3 @@
 module Backports
-  VERSION = "3.21.0" unless Backports.constants.include? :VERSION # the guard is against a redefinition warning that happens on Travis
+  VERSION = "3.22.0" unless Backports.constants.include? :VERSION # the guard is against a redefinition warning that happens on Travis
 end
