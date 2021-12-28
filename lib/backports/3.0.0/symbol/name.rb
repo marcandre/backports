ca6cd73405258683e5adb94a500ba4d74503e47b
Marc-André Lafortune <github@marc-andre.ca>
2021-12-28 10:41:22 -0500

    Improve compatibility for Symbol#name

--- lib/backports/3.0.0/symbol/name.rb
+++ lib/backports/3.0.0/symbol/name.rb
@@ -1,11 +1,21 @@
 unless Symbol.method_defined? :name
-  def Backports.symbol_names
-    @symbol_names ||= ObjectSpace::WeakMap.new
-  end
+  if ((ObjectSpace::WeakMap.new[:test] = :test) rescue false)
+    # WeakMaps accept symbols only in Ruby 2.7+
+    def Backports.symbol_names
+      @symbol_names ||= ObjectSpace::WeakMap.new
+    end
 
-  class Symbol
-    def name
-      Backports.symbol_names[self] ||= to_s.freeze
+    class Symbol
+      def name
+        Backports.symbol_names[self] ||= to_s.freeze
+      end
+    end
+  else
+    # For earlier Rubies, we can't pool their strings
+    class Symbol
+      def name
+        to_s.freeze
+      end
     end
   end
 end
