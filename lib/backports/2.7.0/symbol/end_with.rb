marcandre/backports

commit 47d0445547c1dd9de70e58b38e3e31294e3f9b8d

ruby-2.7: Symbol#end_with?

lib/backports/2.7.0/symbol/end_with.rb
@@ -1,0 +1,9 @@
+require 'backports/1.8.7/string/end_with' unless String.method_defined? :end_with
+
+unless Symbol.method_defined?(:end_with?)
+  class Symbol
+    def end_with?(*suffixes)
+      to_s.end_with?(*suffixes)
+    end
+  end
+end

lib/backports/2.7.0/symbol.rb
@@ -1,0 +1,3 @@
+require 'backports/tools/require_relative_dir'
+
+Backports.require_relative_dir

commit 94999eeee2b66dd71c95f0b989233c668eccb17f

Fix String#end_with? guard clause in Symbol
Fixes minor typo introduced in https://github.com/marcandre/backports/pull/171
The issue isn't noticeable as `backports/1.8.7/string/end_with` has proper guard.

lib/backports/2.7.0/symbol/end_with.rb
@@ -1,4 +1,4 @@
-require 'backports/1.8.7/string/end_with' unless String.method_defined? :end_with
+require 'backports/1.8.7/string/end_with' unless String.method_defined? :end_with?
 
 unless Symbol.method_defined?(:end_with?)
   class Symbol