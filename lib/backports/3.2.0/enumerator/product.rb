4f5b6dbe343b8be3a664a82b7f0577b2d2e28760
Marc-André Lafortune <github@marc-andre.ca>
2024-03-05 15:05:52 -0500

    Update lib/backports/3.2.0/enumerator/product.rb
    Let's not go out of our way to return senseless results.
    See https://github.com/ruby/spec/pull/1140

--- lib/backports/3.2.0/enumerator/product.rb
+++ lib/backports/3.2.0/enumerator/product.rb
@@ -56,7 +56,7 @@ unless Enumerator.method_defined? :product
       @__enums.each do |enum|
         return nil unless enum.respond_to?(:size)
         size = enum.size
-        return size if size == nil || size == Float::INFINITY || size == -Float::INFINITY
+        return size if size == nil || size == Float::INFINITY
         return nil unless size.kind_of?(Integer)
         total_size *= size
       end
